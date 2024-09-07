13e3437d09447f092c59b7e79374b4966dd3b25e
Grayson <Grayson@Desktop-7F7HF>
2024-09-07 13:41:41 -0400

    Add project files.

---
+++ LookAt.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using Photon.Pun;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace NameTags
+{
+    internal class LookAt : MonoBehaviour
+    {
+        public GameObject toStare = null;
+        public UnityEngine.UI.Text text = null;
+        public VRRig who = null;
+
+        void LateUpdate()
+        {
+            if (who.enabled)
+            {
+                toStare.transform.LookAt(GorillaTagger.Instance.headCollider.transform.position);
+                text.text = GetPlayerFromVRRig(who).NickName;
+                text.color = who.mainSkin.material.name.Contains("fected") ? new Color(1f, 0.5f, 0f, 1) : who.playerColor;
+            } else
+            {
+                text.text = "null";
+            }
+        }
+
+        public NetPlayer GetPlayerFromVRRig(VRRig p)
+        {
+            //return GetPhotonViewFromVRRig(p).Owner;
+            return p.Creator;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,45 @@
+﻿using BepInEx;
+using System;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+
+namespace NameTags
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Update()
+        {
+            foreach (VRRig vrrig in GorillaParent.instance.vrrigs)
+            {
+                if (vrrig != GorillaTagger.Instance.offlineVRRig && !vrrig.headMesh.transform.Find("NameTag(Clone)"))
+                {
+                    GameObject literallynametag = LoadAsset("NameTag");
+                    print(literallynametag.name);
+                    literallynametag.transform.SetParent(vrrig.headMesh.transform, false);
+                    LookAt a = literallynametag.AddComponent<LookAt>();
+                    a.toStare = literallynametag.transform.Find("Canvas").gameObject;
+                    a.text = a.toStare.transform.Find("Name").gameObject.GetComponent<UnityEngine.UI.Text>();
+                    a.who = vrrig;
+                }
+            }
+        }
+
+        static AssetBundle assetBundle = null;
+        public static GameObject LoadAsset(string assetName)
+        {
+            GameObject gameObject = null;
+
+            if (assetBundle == null)
+            {
+                Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("NameTags.Resources.nametag");
+                assetBundle = AssetBundle.LoadFromStream(stream);
+                stream.Close();
+            }
+            gameObject = Instantiate<GameObject>(assetBundle.LoadAsset<GameObject>(assetName));
+
+            return gameObject;
+        }
+    }
+}
